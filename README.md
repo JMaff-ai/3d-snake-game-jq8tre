14caed3e801d8f0f806c16c92229dac2012a2181
JMaff-ai <jmaffettone8802@gmail.com>
2021-04-04 03:24:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3d-snake-game-jq8tre
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/3d-snake-game-jq8tre)
---
+++ Snake.ts
@@ -0,0 +1,274 @@
+import { useCallback } from "react";
+import display from "./display";
+
+// place your code on line 5 above the export statement below
+
+class Snake {
+    private currentPosition: number;
+    private currentDirection: number;
+newPoint: any;
+startposition: any;
+size: any;
+currentParts: number;
+break: any;
+    
+    public constructor() {
+      this.currentParts == 0;
+      this.currentDirection == 1;
+      this.startposition == Point;
+      this.size == Point;
+      for( let newPoint, thissize == 1;);
+    }
+    public turn() {
+      this.currentDirection *= -1;
+    }
+    public move(squares: number) {
+      this.currentPosition += this.currentDirection * squares;
+      this.position ; this.newPoint;
+      if(this.newPoint = this.position.x+1,this.position.y-1) display(this.position);
+      else (this.newPoint = this.position.x-1,this.position.y+1); display(this.position);
+      for(let Point = 1; Point<1 ; Point=Point + 1)display(Point);
+      for(let newPoint = 0; newPoint<0 ; newPoint=newPoint+0)display(newPoint);
+      this.currentParts == 0
+    }
+    
+    public get position() {
+      return this.currentPosition;
+    }
+    public get direction() {
+      return Snake;
+    }
+    public Collidable() {
+      this.Date(this.move)
+      if(this.type != "snake") return true;
+      else return false;
+    }
+    private isCurrentlyActive(){
+      for(let isCurrentlyActive = true;)
+     this.break(isCurrentlyActive)
+    }
+    public get isActive(){
+      return this.isCurrentlyActive;
+    }
+    public get type(){
+      return "snake";
+    }
+Date(move: any) {
+throw new Error("Method not implemented.");
+}
+    }
+    class Point {
+static grow: any;
+[x: string]: any;
+      private xcoord: number;
+      private ycoord: number;
+
+      public constructor() {
+        this.xcoord == 0;
+        this.ycoord == 0;
+
+      }
+      public newPoint() {
+      this.xcoord += 0;
+      this.ycoord += 0;
+    }
+    public turnLeft() {
+      if(this.xcoord == 90) display(this.currentDirection);
+      else if(this.xcoord == 180) display(this.currentDirection);
+      else if(this.xcoord == 270) display(this.currentDirection);
+      else display(this.currentDirection);
+    }
+    public turnRight() {
+      if(this.ycoord == -90) display(this.currentDirection);
+      else if(this.ycoord == -180) display(this.currentDirection);
+      else if(this.ycoord == -270) display(this.currentDirection);
+      else display(this.currentDirection);
+    }
+    public equals(p) {
+      if (y == x) {
+        return true
+      }
+    }
+    }
+    class WorldModel {
+sna: Snake;
+      set snake(s:Snake) {
+        this.sna = s;
+      }
+      public constructor() {
+        let addSnake = Snake;
+        let addView = View;
+        
+      }
+      public get width() {
+        return Snake;
+      }
+      public get height() {
+        return Snake;
+      }
+      public WorldModelupdate(_steps: Snake) {
+        return this.allSnakes;
+      }
+      public view = null;
+      public update(View: any) {
+        for(let allSnakes = 0 ; allSnakes < 0; );
+        return this.allViews;
+      }
+      const allSnakes = [];
+      const allViews = [];
+      const addSnakes = Snake;
+      const addViews = View;
+      
+    }
+class SnakeController{
+  public constructor() {
+    let World = Number; 
+    let Snake = Number;
+    let snakeWorld = World;
+    let siltherer = Snake;
+  }
+  public turnSnakeLeft() {
+    let turnLeft = Snake;
+  }
+  public turnSnakeRight() {
+    let turnRight = Snake;
+  }
+  public get snakePosition() {
+    return Snake;
+  }
+  public get WorldWidth() {
+    return WorldModel;
+  }
+  public get WorldHeight() {
+    return WorldModel;
+  }
+class Player extends AvoidWallsPlayer {
+  public constructor() {
+   super(SnakeController);
+  }
+  public makeTurn() {
+    return SnakeController;
+  }
+}
+const View = Symbol('View');
+
+export default View;
+
+class CanvasView {
+view: any;
+  public constructor() {
+    let WorldWidth = 3;
+    let Worldheight = 10;
+    let canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const ctx = canvas.getContext('2d');
+    display(WorldModel);
+    display(WorldWidth);
+    display(Worldheight);
+  } 
+  public setView(newView) {
+    this.view = newView;
+  }
+  public CanvasView() {
+    let view = WorldModel;
+    return Date();
+  }
+}
+interface InputHandler{Player: any} {
+  const madeLeftMove = true;
+  const madeRightMove = true;
+  const resetLeftMove = 0;
+  const resetRightMove = 0;
+}
+class LRKeyInputHandler {
+  public constructor() {
+    const wasLeftArrowPushed = false;
+    const wasRightArrowPushed = false;
+  }
+  public textarea = document.getElementById('test-target');
+consoleLog = document.getElementById('console-log');
+btnClearConsole = document.getElementById('btn-clear-console');
+public addEventListener('keydown', (Right => {
+  if (!Right.repeat)
+    (`Key "${Right.key}" pressed  [event: keydown]`);
+  else
+    (`Key "${Right.key}" repeating  [event: keydown]`);
+}
+}
+class HumanPlayer {
+  public constructor() {
+    let SnakeController = 0;
+    let InputHandler = 0;
+    if (SnakeController = turnSnakeleft) (InputHandler = makeMoveLeft);
+    return true;
+  }
+}
+class GameController {
+  private constructor(){
+  private WorldModel; 0;
+  private player1: 0
+  private player2: 0}
+  let run = 0 => {
+    console.log(0);
+    requestAnimationFrame(run);
+  };
+requestAnimationFrame(run);
+}
+
+interface Actor{
+  addQuanity(arg0: Actor);
+}
+interface Collidable({arg0}){
+  this.didCollide(arg0)
+}
+class food {
+  public constructor(x,y) {
+    x = new Point; {
+      let isCurrentlyActive = true;{
+        let eat = false;
+        Point.grow(2,4);
+      }
+    }
+  }
+  public get position(){
+    return CaretPosition;
+  }
+}
+interface CollidableHandler{
+  applyAction: any;
+}
+interface SnakeFoodCollisionHandler{
+  main(eat,grow);
+}
+interface SnakeCollisionHandler{
+  applyaction: this.break: any;
+}
+class ActorCollisonHandler {
+  public constructor() {
+    const pairs = new Map();
+  }
+  private toKey{
+    collidedType == 0;
+    const createArrayIterator = function*(arr:[]) {
+      ( collidedType + "," + collidertype)
+    }
+    addCollisonEvent(colliderType, collidedType, actionApplicator)
+    const pairsMap = new Map();
+    pairsMap.set(collidertype, actionapplier)
+
+    pairsMap.has('bar');
+
+    this.applyCollisionAction(collider, collided);
+  }
+}
+class ArrayIterator{
+  public constructor(){
+    
+  }
+}
+
+
+
+let Gamecontroller = HumanPlayer + AvoidWallsPlayer;
+return GameController;
+export: any default Snake;
